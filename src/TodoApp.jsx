47d91e4db12dfb760c44c24fc596f937ca67cda1
DANIEL21NAULA <163338712+DANIEL21NAULA@users.noreply.github.com>
2024-06-04 15:02:48 -0500

    RKT redux

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useGetTodoQuery } from './store/apis';
+// import { useGetTodosQuery } from './store/apis';
 
-export const TodoApp = () => (
-  <>
-    <h1>TODOs - RTK Query</h1>
-    <hr />
-    <h4>IsLoading...</h4>
-    <pre>...</pre>
-    <button type="button">
-      Next Todo
-    </button>
-  </>
-);
+export const TodoApp = () => {
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const [todoId, setTodoId] = useState(1);
+  const { data: todo, isLoading } = useGetTodoQuery(todoId);
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  };
+  const previosTodo = () => {
+    if (todoId > 1) {
+      setTodoId(todoId - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>TODOs - RTK Query</h1>
+      <h4>
+        isLoading:
+        {' '}
+        {isLoading ? 'true' : 'false'}
+      </h4>
+      <hr />
+      <h4>IsLoading...</h4>
+      <pre>{JSON.stringify(todo)}</pre>
+      {/* <ul>
+        {
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <strong>
+                {todo.completed ? 'Done' : 'Pending'}
+                {' '}
+              </strong>
+              {todo.title}
+            </li>
+          ))
+        }
+      </ul> */}
+      <button
+        type="button"
+        onClick={previosTodo}
+      >
+        Previos Todo
+      </button>
+      <button
+        type="button"
+        onClick={nextTodo}
+      >
+        Next Todo
+      </button>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { TodoApp } from './TodoApp';
 // import App from './App.jsx';
 import './index.css';
-import { PokemonApp } from './PokemonApp.jsx';
+// import { PokemonApp } from './PokemonApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>,
 );
